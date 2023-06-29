5862a05579b559391c46925667e5a53386fde133
miguel2512-UTF <miguelangelwilchez1001@gmail.com>
2023-06-28 22:49:54 -0500

    First responsive for viewports of >900

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,13 @@
 let menu_icon = document.querySelector(".menu-icon")
+let btn_close_sidebar = document.querySelector(".btn-close-sidebar")
 
 menu_icon.addEventListener("click", (e) => {
+    document.querySelector(".header").classList.toggle("content-wrapper")
+    document.querySelector(".content-body").classList.toggle("content-wrapper")
+    document.querySelector(".sidebar").classList.toggle("sidebar-wrapper")
+})
+
+btn_close_sidebar.addEventListener("click", (e) => {
     document.querySelector(".header").classList.toggle("content-wrapper")
     document.querySelector(".content-body").classList.toggle("content-wrapper")
     document.querySelector(".sidebar").classList.toggle("sidebar-wrapper")
